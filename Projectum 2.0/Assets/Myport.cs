b85a974a69bda29a63fe44a86fa2b3a9d9bd1b66
Grandman2003 <goykhman200316@gmail.com>
2020-04-09 16:28:58 +0300

    It is time to learn

---
+++ Projectum 2.0/Assets/Myport.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Myport : MonoBehaviour
+{
+
+    public Transform vrCamera;
+    public bool moveForward;
+    // Start is called before the first frame update
+    private CharacterController cc;
+    private GameObject teleport;
+
+    // Use this for initialization
+    void Start()
+    {
+        teleport = GetComponent<GameObject>();
+        cc = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+     
+
+    }
+    public void Teleportic()
+    {
+        Vector3 forward = vrCamera.TransformDirection(Vector3.forward);
+        cc.Move(forward*4);
+    }
+}
---
+++ Projectum 2.0/Assets/sho.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class sho : MonoBehaviour
+{
+    public GameObject InfoObject;
+    public GameObject InfoObject1;
+    private bool Show = false;
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    public void ShowAndHideInfo()
+    {
+        if (!Show)
+        {
+            InfoObject1.SetActive(true);
+            InfoObject1.SetActive(false);
+            InfoObject.SetActive(true);
+            Show = true;
+        }else{
+            InfoObject1.SetActive(true);
+            InfoObject1.SetActive(false);
+            InfoObject.SetActive(false);
+            Show = false;
+        }
+    }
+}
